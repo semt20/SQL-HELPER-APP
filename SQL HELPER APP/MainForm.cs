9034ef1fc4f07c98a5ddb4228c8bbd361784fee2
semt20 <semt20@gmail.com>
2019-07-03 15:23:08 +0300

    1

--- SQL HELPER APP/MainForm.cs
+++ SQL HELPER APP/MainForm.cs
@@ -11,7 +11,7 @@ using DevExpress.XtraEditors;
 using DevExpress.XtraReports.UI;
 using System.Data.SqlClient;
 using DevExpress.XtraBars;
-using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Ribbon; 
 using DevExpress.XtraPrinting.Preview;
 using SQL_HELPER_APP.Properties;
 using DevExpress.LookAndFeel;
@@ -21,7 +21,7 @@ namespace SQL_HELPER_APP
 {
     public partial class MainForm : DevExpress.XtraEditors.XtraForm
     {
-        public MainForm()
+        public MainForm() 
         {
             InitializeComponent();
         }
