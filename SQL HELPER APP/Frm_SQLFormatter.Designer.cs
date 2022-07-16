2d49bb56bdb9e47e3161fcafb648fc7f108bc517
semt20 <semt20@gmail.com>
2022-07-16 13:39:14 +0300

    SQL Formatter UI Improvements...

--- SQL HELPER APP/Frm_SQLFormatter.Designer.cs
+++ SQL HELPER APP/Frm_SQLFormatter.Designer.cs
@@ -48,6 +48,7 @@
             this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
             this.me_SQLSource = new DevExpress.XtraEditors.MemoEdit();
             this.rec_SQLDestination = new DevExpress.XtraRichEdit.RichEditControl();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ce_htmlColoring.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ce_uppercaseKeywords.Properties)).BeginInit();
@@ -67,33 +68,23 @@
             this.splitContainerControl1.Panel2.SuspendLayout();
             this.splitContainerControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.me_SQLSource.Properties)).BeginInit();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.ce_htmlColoring);
-            this.panel1.Controls.Add(this.ce_uppercaseKeywords);
-            this.panel1.Controls.Add(this.ce_breakJoinOnSections);
-            this.panel1.Controls.Add(this.ce_expandBetweenConditions);
-            this.panel1.Controls.Add(this.ce_expandCaseStatements);
-            this.panel1.Controls.Add(this.ce_expandBooleanExpressions);
-            this.panel1.Controls.Add(this.ce_spaceAfterExpandedComma);
-            this.panel1.Controls.Add(this.ce_trailingCommas);
-            this.panel1.Controls.Add(this.ce_expandCommaLists);
-            this.panel1.Controls.Add(this.labelControl1);
-            this.panel1.Controls.Add(this.se_spacesPerTab);
-            this.panel1.Controls.Add(this.lc_SDFPath);
             this.panel1.Controls.Add(this.sb_FormatSql);
+            this.panel1.Controls.Add(this.panel4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1019, 122);
+            this.panel1.Size = new System.Drawing.Size(1207, 122);
             this.panel1.TabIndex = 2;
             // 
             // ce_htmlColoring
             // 
             this.ce_htmlColoring.EditValue = true;
-            this.ce_htmlColoring.Location = new System.Drawing.Point(225, 95);
+            this.ce_htmlColoring.Location = new System.Drawing.Point(176, 92);
             this.ce_htmlColoring.Name = "ce_htmlColoring";
             this.ce_htmlColoring.Properties.Caption = "html Coloring";
             this.ce_htmlColoring.Size = new System.Drawing.Size(161, 20);
@@ -101,7 +92,7 @@
             // 
             // ce_uppercaseKeywords
             // 
-            this.ce_uppercaseKeywords.Location = new System.Drawing.Point(225, 74);
+            this.ce_uppercaseKeywords.Location = new System.Drawing.Point(176, 71);
             this.ce_uppercaseKeywords.Name = "ce_uppercaseKeywords";
             this.ce_uppercaseKeywords.Properties.Caption = "uppercase Keywords";
             this.ce_uppercaseKeywords.Size = new System.Drawing.Size(161, 20);
@@ -109,7 +100,7 @@
             // 
             // ce_breakJoinOnSections
             // 
-            this.ce_breakJoinOnSections.Location = new System.Drawing.Point(225, 55);
+            this.ce_breakJoinOnSections.Location = new System.Drawing.Point(176, 52);
             this.ce_breakJoinOnSections.Name = "ce_breakJoinOnSections";
             this.ce_breakJoinOnSections.Properties.Caption = "break Join On Sections";
             this.ce_breakJoinOnSections.Size = new System.Drawing.Size(161, 20);
@@ -117,7 +108,7 @@
             // 
             // ce_expandBetweenConditions
             // 
-            this.ce_expandBetweenConditions.Location = new System.Drawing.Point(225, 34);
+            this.ce_expandBetweenConditions.Location = new System.Drawing.Point(176, 31);
             this.ce_expandBetweenConditions.Name = "ce_expandBetweenConditions";
             this.ce_expandBetweenConditions.Properties.Caption = "expand Between Conditions";
             this.ce_expandBetweenConditions.Size = new System.Drawing.Size(161, 20);
@@ -125,7 +116,7 @@
             // 
             // ce_expandCaseStatements
             // 
-            this.ce_expandCaseStatements.Location = new System.Drawing.Point(225, 12);
+            this.ce_expandCaseStatements.Location = new System.Drawing.Point(176, 9);
             this.ce_expandCaseStatements.Name = "ce_expandCaseStatements";
             this.ce_expandCaseStatements.Properties.Caption = "expand Case Statements";
             this.ce_expandCaseStatements.Size = new System.Drawing.Size(161, 20);
@@ -133,23 +124,23 @@
             // 
             // ce_expandBooleanExpressions
             // 
-            this.ce_expandBooleanExpressions.Location = new System.Drawing.Point(12, 95);
+            this.ce_expandBooleanExpressions.Location = new System.Drawing.Point(3, 92);
             this.ce_expandBooleanExpressions.Name = "ce_expandBooleanExpressions";
             this.ce_expandBooleanExpressions.Properties.Caption = "expand Boolean Expressions";
-            this.ce_expandBooleanExpressions.Size = new System.Drawing.Size(179, 20);
+            this.ce_expandBooleanExpressions.Size = new System.Drawing.Size(167, 20);
             this.ce_expandBooleanExpressions.TabIndex = 7;
             // 
             // ce_spaceAfterExpandedComma
             // 
-            this.ce_spaceAfterExpandedComma.Location = new System.Drawing.Point(12, 74);
+            this.ce_spaceAfterExpandedComma.Location = new System.Drawing.Point(3, 71);
             this.ce_spaceAfterExpandedComma.Name = "ce_spaceAfterExpandedComma";
             this.ce_spaceAfterExpandedComma.Properties.Caption = "space After Expanded Comma";
-            this.ce_spaceAfterExpandedComma.Size = new System.Drawing.Size(179, 20);
+            this.ce_spaceAfterExpandedComma.Size = new System.Drawing.Size(167, 20);
             this.ce_spaceAfterExpandedComma.TabIndex = 6;
             // 
             // ce_trailingCommas
             // 
-            this.ce_trailingCommas.Location = new System.Drawing.Point(12, 55);
+            this.ce_trailingCommas.Location = new System.Drawing.Point(3, 52);
             this.ce_trailingCommas.Name = "ce_trailingCommas";
             this.ce_trailingCommas.Properties.Caption = "trailing Commas";
             this.ce_trailingCommas.Size = new System.Drawing.Size(137, 20);
@@ -157,7 +148,7 @@
             // 
             // ce_expandCommaLists
             // 
-            this.ce_expandCommaLists.Location = new System.Drawing.Point(12, 34);
+            this.ce_expandCommaLists.Location = new System.Drawing.Point(3, 31);
             this.ce_expandCommaLists.Name = "ce_expandCommaLists";
             this.ce_expandCommaLists.Properties.Caption = "expand Comma Lists";
             this.ce_expandCommaLists.Size = new System.Drawing.Size(137, 20);
@@ -165,7 +156,7 @@
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(12, 15);
+            this.labelControl1.Location = new System.Drawing.Point(3, 12);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(78, 13);
             this.labelControl1.TabIndex = 3;
@@ -178,7 +169,7 @@
             0,
             0,
             0});
-            this.se_spacesPerTab.Location = new System.Drawing.Point(96, 12);
+            this.se_spacesPerTab.Location = new System.Drawing.Point(87, 9);
             this.se_spacesPerTab.Name = "se_spacesPerTab";
             this.se_spacesPerTab.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
@@ -187,7 +178,7 @@
             // 
             // lc_SDFPath
             // 
-            this.lc_SDFPath.Location = new System.Drawing.Point(115, 15);
+            this.lc_SDFPath.Location = new System.Drawing.Point(106, 12);
             this.lc_SDFPath.Name = "lc_SDFPath";
             this.lc_SDFPath.Size = new System.Drawing.Size(0, 13);
             this.lc_SDFPath.TabIndex = 1;
@@ -195,11 +186,11 @@
             // sb_FormatSql
             // 
             this.sb_FormatSql.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.sb_FormatSql.Dock = System.Windows.Forms.DockStyle.Right;
+            this.sb_FormatSql.Dock = System.Windows.Forms.DockStyle.Left;
             this.sb_FormatSql.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("sb_FormatSql.ImageOptions.SvgImage")));
-            this.sb_FormatSql.Location = new System.Drawing.Point(797, 0);
+            this.sb_FormatSql.Location = new System.Drawing.Point(345, 0);
             this.sb_FormatSql.Name = "sb_FormatSql";
-            this.sb_FormatSql.Size = new System.Drawing.Size(222, 122);
+            this.sb_FormatSql.Size = new System.Drawing.Size(129, 122);
             this.sb_FormatSql.TabIndex = 0;
             this.sb_FormatSql.Text = "Format Sql";
             this.sb_FormatSql.Click += new System.EventHandler(this.sb_FormatSql_Click);
@@ -209,7 +200,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(0, 671);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1019, 29);
+            this.panel2.Size = new System.Drawing.Size(1207, 29);
             this.panel2.TabIndex = 3;
             // 
             // panel3
@@ -218,7 +209,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 122);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1019, 549);
+            this.panel3.Size = new System.Drawing.Size(1207, 549);
             this.panel3.TabIndex = 4;
             // 
             // splitContainerControl1
@@ -236,7 +227,7 @@
             // 
             this.splitContainerControl1.Panel2.Controls.Add(this.rec_SQLDestination);
             this.splitContainerControl1.Panel2.Text = "Panel2";
-            this.splitContainerControl1.Size = new System.Drawing.Size(1019, 549);
+            this.splitContainerControl1.Size = new System.Drawing.Size(1207, 549);
             this.splitContainerControl1.SplitterPosition = 470;
             this.splitContainerControl1.TabIndex = 0;
             // 
@@ -260,15 +251,35 @@
             this.rec_SQLDestination.Options.DocumentCapabilities.ParagraphBorders = DevExpress.XtraRichEdit.DocumentCapability.Disabled;
             this.rec_SQLDestination.Options.HorizontalRuler.Visibility = DevExpress.XtraRichEdit.RichEditRulerVisibility.Hidden;
             this.rec_SQLDestination.Options.VerticalRuler.Visibility = DevExpress.XtraRichEdit.RichEditRulerVisibility.Hidden;
-            this.rec_SQLDestination.Size = new System.Drawing.Size(539, 549);
+            this.rec_SQLDestination.Size = new System.Drawing.Size(727, 549);
             this.rec_SQLDestination.TabIndex = 0;
             this.rec_SQLDestination.Views.SimpleView.WordWrap = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.labelControl1);
+            this.panel4.Controls.Add(this.ce_htmlColoring);
+            this.panel4.Controls.Add(this.lc_SDFPath);
+            this.panel4.Controls.Add(this.ce_uppercaseKeywords);
+            this.panel4.Controls.Add(this.se_spacesPerTab);
+            this.panel4.Controls.Add(this.ce_breakJoinOnSections);
+            this.panel4.Controls.Add(this.ce_expandCommaLists);
+            this.panel4.Controls.Add(this.ce_expandBetweenConditions);
+            this.panel4.Controls.Add(this.ce_trailingCommas);
+            this.panel4.Controls.Add(this.ce_expandCaseStatements);
+            this.panel4.Controls.Add(this.ce_spaceAfterExpandedComma);
+            this.panel4.Controls.Add(this.ce_expandBooleanExpressions);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(345, 122);
+            this.panel4.TabIndex = 13;
             // 
             // Frm_SQLFormatter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1019, 700);
+            this.ClientSize = new System.Drawing.Size(1207, 700);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
@@ -276,7 +287,6 @@
             this.Text = "SQL Formatter";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ce_htmlColoring.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ce_uppercaseKeywords.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ce_breakJoinOnSections.Properties)).EndInit();
@@ -295,6 +305,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
             this.splitContainerControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.me_SQLSource.Properties)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -319,6 +331,7 @@
         private DevExpress.XtraEditors.CheckEdit ce_uppercaseKeywords;
         private DevExpress.XtraEditors.CheckEdit ce_htmlColoring;
         private DevExpress.XtraRichEdit.RichEditControl rec_SQLDestination;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
